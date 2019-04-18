42bbdbb7234978807024452518aebd18d5409944
Zac Smith <zrs3141592@gmail.com>
2019-04-18 11:16:02 -0600

    change to arrow functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,23 +3,34 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+
+let myFunction = () => 'Function was invoked!';
+console.log(myFunction());
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+let anotherFunction = (param) => param;
+console.log(anotherFunction('happy'));
+console.log(anotherFunction(9));
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let add = (param1, param2) => param1 + param2;
+console.log(add(4,5));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(10,3));
 
 // Stretch
 
