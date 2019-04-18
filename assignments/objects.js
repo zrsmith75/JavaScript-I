b4ebf94b1b7d593c91a0b2e409470bb24cddeecb
Zac Smith <zrs3141592@gmail.com>
2019-04-17 18:08:12 -0600

    fix naming convention

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,39 +19,41 @@ const example = {
 
 // Write your intern objects here:
 
-const person0 = {
-  "id": 0,
-  "name": "Mitzi",
-  "email": "mmelloy0@psu.edu",
-  "gender": "F"
+const mitzi = {
+  id: 0,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
 }
 
-const person1 = {
-  "id": 1,
-  "name": "Kennan",
-  "email": "kdiben1@tinypic.com",
-  "gender": "M"
+const kennan = {
+  id: 1,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function() { return `My name is ${this.name}!`},
 }
 
-const person2 = {
-  "id": 2,
-  "name": "Keven",
-  "email": "kmummery2@wikimedia.org",
-  "gender": "M"
+const keven = {
+  id: 2,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
 }
 
-const person3 = {
-  "id": 3,
-  "name": "Gannie",
-  "email": "gmartinson3@illinois.edu",
-  "gender": "M"
+const gannie = {
+  id: 3,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
 }
 
-const person4 = {
-  "id": 4,
-  "name": "Antonietta",
-  "email": "adaine5@samsung.com",
-  "gender": "F"
+const antonietta = {
+  id: 4,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+  multiplyNums: function(num1, num2) { return num1 * num2; },
 }
 
 
@@ -59,26 +61,31 @@ const person4 = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(person0.name);
+console.log(mitzi.name);
 
 // Kennan's ID
-console.log(person1.id);
+console.log(kennan.id);
 
 // Keven's email
-console.log(person1.email);
+console.log(keven.email);
 
 // Gannie's name
-console.log(person3.name);
+console.log(gannie.name);
 
 // Antonietta's Gender
-console.log(person4.gender);
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(antonietta.multiplyNums(3,4));
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -89,11 +96,27 @@ console.log(person4.gender);
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: 'Susan',
+  age: 70,
+  speak: function() { return `My name is ${this.name}!`},
+  child: {
+    name: 'George',
+    age: 50, 
+    grandchild: {
+      name: 'Sam',
+      age: 30,
+    }
+  } 
+ } 
+
+ parent_grp = Object.assign(parent, child);
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
 
